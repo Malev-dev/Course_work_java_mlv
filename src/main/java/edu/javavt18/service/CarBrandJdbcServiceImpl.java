a7139014256b4ba1cabd5f3aafa03b81a74aa285
Malev-dev <malevanydrg1@ukr.net>
2020-12-14 21:33:02 +0200

    added the final result of the project

--- src/main/java/edu/javavt18/service/CarBrandJdbcServiceImpl.java
+++ src/main/java/edu/javavt18/service/CarBrandJdbcServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import java.util.List;
+
 @Service("carBrandJdbcService")
 public class CarBrandJdbcServiceImpl implements CarBrandService {
     @Autowired
